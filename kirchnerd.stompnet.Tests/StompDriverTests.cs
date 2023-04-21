6524734b20741ae52dbff97422f077325ef547ac
Enno Kirchner <kirchner@adremes.com>
2023-04-21 09:21:09 +0200

    Bug fix StompMarshallerTest

--- kirchnerd.stompnet.Tests/StompDriverTests.cs
+++ kirchnerd.stompnet.Tests/StompDriverTests.cs
@@ -10,7 +10,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace kirchnerd.StompNet.Tests;
 
 /// <remarks>
-/// Run docker container run -it --name rabbitmq-stomp -p 15672:15672 -p 5672:5672 -p 61613:61613 pcloud/rabbitmq-stomp.
+/// Run ``docker container run -it --name rabbitmq-stomp -p 15672:15672 -p 5672:5672 -p 61613:61613 pcloud/rabbitmq-stomp``.
 /// </remarks>
 [TestClass]
 [TestCategory("System")]
